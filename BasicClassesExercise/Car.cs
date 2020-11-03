7807a6b74e3a0b7199b5008b8b15015feb713ca9
[larissa bottefield] <[larissabott3@gmail.com]>
2020-11-03 14:29:02 -0600

    car class created

---
+++ BasicClassesExercise/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicClassesExercise
+{
+    public class Car
+    {
+        public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+        public Car() //- a special member method that has the same as its class
+        {
+        }
+
+        //fields
+        //properties
+        //methods
+
+        //create a make property of tyoe string that is public
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+
+    }
+} 
+    
---
+++ BasicClassesExercise/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace BasicClassesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myCar = new Car();
+            myCar.Make = "Honda";
+            myCar.Model = "CRV";
+            myCar.Year = 2002;
+
+            var chevy = new Car()
+            {
+                Make = "Chevy",
+                Model = "Blaze",
+                Year = 2003
+            };
+
+            var mazda = new Car("Mazda", "CX-5", 2016);
+
+            var CarList = new List<Car>() { myCar, mazda, chevy };
+
+            foreach(var vehicle in CarList)
+            {
+                Console.WriteLine($"{vehicle.Make} {vehicle.Model} {vehicle.Year}");
+            }
+        }
+    }
+}
